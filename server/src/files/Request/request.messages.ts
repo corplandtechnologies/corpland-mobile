05c0de99955f85a43876d24c7c4fde2674e2ca0b
asantesylvesterkwame <intelligentson17@gmail.com>
2024-05-01 12:35:29 +0000

    cleaned it up

--- server/src/files/Request/request.messages.ts
+++
@@ -1,8 +0,0 @@
-export const messages = {
-  REQUEST_CREATED: "Request created successfully",
-  REQUEST_UPDATED: "Request updated successfully",
-  REQUEST_DELETED: "Request deleted successfully",
-  REQUEST_NOT_FOUND: "Request not found",
-  REQUEST_ALREADY_EXISTS: "Request already exists",
-  REQUEST_ERROR: "Something went wrong!",
-};
--- server/src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import requestRoutes from "../files/Request/request.routes";
-
-const useRoutes = (app: express.Application) => {
-  const base = `/api/v1`;
-  app.use(`${base}/requests`, requestRoutes);
-};
-
-export default useRoutes;
