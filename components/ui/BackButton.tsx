0b3dcdeebb316e279a2653a566f024e1fa5beddd
asantesylvesterkwame <intelligentson17@gmail.com>
2024-07-10 13:49:39 +0000

    fixed back button

--- components/ui/BackButton.tsx
+++ components/ui/BackButton.tsx
@@ -18,7 +18,7 @@ const BackButton: FC<BackButtonProps> = ({ details }) => {
     : {};
   return (
     <TouchableOpacity
-      onPress={() => navigation.goBack()}
+      onPress={() => navigation.goBack() || navigation.navigate("Home")}
       style={[styles.backButton, detailStyles]}>
       <Icon
         name="arrow-back"
