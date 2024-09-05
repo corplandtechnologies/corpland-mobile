c0dd6a68dfa32db7d92087e175f93d8a28d13f84
asantesylvesterkwame <intelligentson17@gmail.com>
2024-09-05 03:13:35 +0000

    fixed api

--- api/api.ts
+++ api/api.ts
@@ -7,15 +7,15 @@ import { Platform } from "react-native";
 //   withCredentials: true,
 // });
 
-export const API = axios.create({
-  baseURL: "http://192.168.1.106:3000/api/v1",
-  withCredentials: true,
-});
-
 // export const API = axios.create({
-//   baseURL: "https://corpland-backend.onrender.com/api/v1",
+//   baseURL: "http://192.168.1.106:3000/api/v1",
 //   withCredentials: true,
 // });
+
+export const API = axios.create({
+  baseURL: "https://corpland-backend.onrender.com/api/v1",
+  withCredentials: true,
+});
 
 const getToken = async () => {
   return await AsyncStorage.getItem("token");
