1cd76e2a618d9807f4a75d2dadbceb4561197dfa
asantesylvesterkwame <intelligentson17@gmail.com>
2024-10-08 11:38:54 +0000

    fixed the Header App store redirect

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,38 +8,39 @@ import {
 } from "react-native";
 import { COLORS } from "../utils/color";
 import { AppStoreButton, GooglePlayButton } from "react-mobile-app-button";
+import PrimaryButton from "./ui/PrimaryButton";
 
 const Header = () => {
-  const handlePress = () => {
-    if (Platform.OS === "ios") {
-      // Link to Google Forms for iOS users
-      window.location.href =
-        "https://docs.google.com/forms/d/e/1FAIpQLSeZH9Sy3dyyiLy8UkyRMUnN6zQdYTInesldplPPeL0H2fZZ0A/viewform?usp=sf_link";
-    } else {
-      // Link to download page for Android users
-      window.location.href =
-        "https://dl.dropboxusercontent.com/scl/fi/r8ikl80ypa80kq58ctcj5/Corpland-1.2.2.apk?rlkey=sm0kue6fmelaxl5jlppomc48s&st=05lav4ko&dl=0";
-    }
-  };
-
   const APKUrl =
     "https://play.google.com/store/apps/details?id=com.corplandtechnologies.corpland&pcampaignid=web_share";
 
   const appleAppURL = "https://apps.apple.com/gh/app/corpland/id6670435009";
+
+  const handlePress = () => {
+    if (Platform.OS === "ios") {
+      // Link to Google Forms for iOS users
+      window.location.href = appleAppURL;
+    } else {
+      // Link to download page for Android users
+      window.location.href = APKUrl;
+    }
+  };
+
   return (
     <View style={styles.header}>
       <Text style={styles.title}>For a better experience...</Text>
       <View style={{ flexDirection: "row" }}>
-        <GooglePlayButton
-          url={APKUrl}
-          theme={"light"}
-          className={"custom-style"}
-        />
-        <AppStoreButton
-          url={appleAppURL}
-          theme={"light"}
-          className={"custom-style"}
-        />
+        {Platform.OS === "ios" ? (
+          <PrimaryButton
+            value="Get it On Apple App Store"
+            onPress={handlePress}
+          />
+        ) : (
+          <PrimaryButton
+            value="Get it On Google Play Store"
+            onPress={handlePress}
+          />
+        )}
       </View>
     </View>
   );
