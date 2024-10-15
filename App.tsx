16284a100b804940a4d35147d33757228d37afa5
asantesylvesterkwame <intelligentson17@gmail.com>
2024-10-15 16:23:15 +0000

    added Notifications with Navigation

--- App.tsx
+++ App.tsx
@@ -236,9 +236,9 @@ function App() {
     );
   }
 
-  if (showTour) {
-    return <WalkthroughTour />;
-  } 
+  // if (showTour) {
+  //   return <WalkthroughTour />;
+  // }
 
   return (
     <>
@@ -249,7 +249,65 @@ function App() {
             <SellerModeProvider>
               <SearchResultsProvider>
                 <ProductProvider>
-                  <NavigationContainer linking={linking}>
+                  <NavigationContainer
+                    linking={{
+                      prefixes: [
+                        prefix,
+                        "https://corpland.corplandtechnologies.com",
+                        "corpland://",
+                      ],
+                      config: {
+                        screens: {
+                          Product: "product/:productId",
+                          // other screens...
+                        },
+                      },
+                      async getInitialURL() {
+                        // First, you may want to do the default deep link handling
+                        // Check if app was opened from a deep link
+                        const url = await Linking.getInitialURL();
+
+                        if (url != null) {
+                          return url;
+                        }
+
+                        // Handle URL from expo push notifications
+                        const response =
+                          await Notifications.getLastNotificationResponseAsync();
+
+                        return response?.notification.request.content.data.url;
+                      },
+                      subscribe(listener) {
+                        const onReceiveURL = ({ url }: { url: string }) =>
+                          listener(url);
+
+                        // Listen to incoming links from deep linking
+                        const eventListenerSubscription =
+                          Linking.addEventListener("url", onReceiveURL);
+
+                        // Listen to expo push notifications
+                        const subscription =
+                          Notifications.addNotificationResponseReceivedListener(
+                            (response) => {
+                              const url =
+                                response.notification.request.content.data.url;
+
+                              // Any custom logic to see whether the URL needs to be handled
+                              //...
+
+                              // Let React Navigation handle the URL
+                              listener(url);
+                            }
+                          );
+
+                        return () => {
+                          // Clean up the event listeners
+                          eventListenerSubscription.remove();
+                          subscription.remove();
+                        };
+                      },
+                    }}
+                  >
                     <Stack.Navigator initialRouteName={"TabNavigator"}>
                       <Stack.Screen
                         name="TabNavigator"
