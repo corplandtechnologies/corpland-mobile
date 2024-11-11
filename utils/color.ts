5c35ab6ca8e58442b06dbad47a0bb13605e3161a
asantesylvesterkwame <intelligentson17@gmail.com>
2024-11-11 13:37:14 +0000

    brought back the previous color

--- utils/color.ts
+++ utils/color.ts
@@ -1,5 +1,5 @@
 export const COLORS = {
-  PRIMARY: "#000000",
+  PRIMARY: "#282828",
   SECONDARY: "#FFFFFF", // White
   TERTIARY: "#AAAAAA", // Light Gray
   COMPLIMENTARY: "#0074D9", // Dark Gray
