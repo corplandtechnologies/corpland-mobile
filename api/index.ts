0d361022e2167ffc2e1b2ea7a00c2c2e18ff0fa1
asantesylvesterkwame <intelligentson17@gmail.com>
2024-11-19 19:38:32 +0000

    made new API switch

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,7 @@ import { Platform } from "react-native";
 // });
 
 export const APIv2 = axios.create({
-  baseURL: "https://corpland-backend-v2.onrender.com/api/v1",
+  baseURL: "https://corpland-backend-new.onrender.com/api/v1",
   withCredentials: true,
 });
 
